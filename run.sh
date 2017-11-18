f6315a95b58d988a4f190fecac4a3fc7dc9653ad
Chen Chiu <chenchiu9@gmail.com>
2017-11-17 21:35:57 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,13 +3,7 @@ unset DEBIAN_FRONTEND
 export LOG=/var/log/jitsi/jvb.log
 
 if [ ! -f "$LOG" ]; then
-	
-	sed 's/#\ create\(.*\)/echo\ create\1 $JICOFO_AUTH_USER $JICOFO_AUTH_DOMAIN $JICOFO_AUTH_PASSWORD/' -i /var/lib/dpkg/info/jitsi-meet-prosody.postinst
-
-	dpkg-reconfigure jitsi-videobridge
-	rm /etc/jitsi/jicofo/config && dpkg-reconfigure jicofo
-	/var/lib/dpkg/info/jitsi-meet-prosody.postinst configure
-	dpkg-reconfigure jitsi-meet
+apt-get -y install jitsi-meet
 
 	touch $LOG && \
 	chown jvb:jitsi $LOG
